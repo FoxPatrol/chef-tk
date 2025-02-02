b04918ef79b15e06b2058e07d91677fb13c22cca
Jose Alves <rajose998@gmail.com>
2025-02-02 22:00:08 +0100

    cleanup generate controller and update readme

--- backend/README.md
+++ backend/README.md
@@ -1,5 +1,19 @@
+# Data Setup
+
 `drizzle` is used to as the ORM between node and postgresql.
 
 `npx drizzle-kit generate`
 
 `npx drizzle-kit migrate`
+
+# Development
+
+`npm i`
+
+`npm run insert-json-data`
+
+`npm run dev`
+
+# Fill Database
+
+Call the endpoint `http://localhost:3000/api/:region/matches/by-riot-id/:name/:tag?forceRefresh=true`
--- backend/src/controllers/generate-response.ts
+++ backend/src/controllers/generate-response.ts
@@ -1,52 +1,13 @@
 import DatabaseConnector from "@db/connector";
 import { getSchemaContents } from "@db/helperFunctions";
-import db from "@db/index";
-import {
-  GenerationConfig,
-  GoogleGenerativeAI,
-  ResponseSchema,
-  SchemaType,
-} from "@google/generative-ai";
 import { Request, Response } from "express";
-
-const apiKey = process.env.GEMINI_API_KEY!;
-const genAI = new GoogleGenerativeAI(apiKey);
-
-const responseSchema: ResponseSchema = {
-  type: SchemaType.OBJECT,
-  properties: {
-    description: {
-      type: SchemaType.STRING,
-      description:
-        "A description of the response in layman terms, 20 words maximum.",
-    },
-    command: {
-      type: SchemaType.STRING,
-      description: "The SQL code.",
-    },
-  },
-  required: ["description", "command"],
-};
-
-const generationConfig: GenerationConfig = {
-  temperature: 0.1,
-  topP: 0.95,
-  topK: 32,
-  maxOutputTokens: 8192,
-  responseMimeType: "application/json",
-  responseSchema: responseSchema,
-};
-
-const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro",
-  generationConfig,
-});
+import { model } from "models/gemini";
 
 class GenerateResponseController {
   postGenerateResponse = async (req: Request, res: Response): Promise<any> => {
     try {
       // Get the prompt from the request body
-      const { prompt, extra } = req.body;
+      const { prompt, extra = [] } = req.body;
 
       if (!prompt) {
         return res.status(400).send({ error: "Prompt is required" });
@@ -64,15 +25,15 @@ class GenerateResponseController {
       const schema = await getSchemaContents();
       const fullPrompt = this.constructPrompt(schema, prompt, extra);
 
-      // Start a new chat session
       const result = await model.generateContent(fullPrompt);
 
       // Extract the response and send it back
       const responseText = result.response.text();
       console.log({ responseText });
 
-      let sqlQuery: string = JSON.parse(responseText).command;
+      let sqlQuery: string = JSON.parse(responseText || "").command;
       sqlQuery = this.removeInnerSingleQuotes(sqlQuery);
+      sqlQuery = this.removeCodeFencing(sqlQuery);
       console.log({ sqlQuery });
 
       const connector = DatabaseConnector.getInstance();
@@ -90,6 +51,10 @@ class GenerateResponseController {
 
   private removeInnerSingleQuotes(str: string): string {
     return str.replace(/(?<=[a-zA-Z])'(?=[a-zA-Z])/g, "");
+  }
+
+  private removeCodeFencing(str: string): string {
+    return str.replace(/^```[\w+]*\n([\s\S]*?)\n```$/gm, "$1");
   }
 
   private constructPrompt(
